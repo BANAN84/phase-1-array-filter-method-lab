5a1742fe678901be308ce5f4508eee504c2a652c
banan <banan.faouri2001@gmail.com>
2023-12-28 21:28:13 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+
+
+function findMatching(driver,name2){
+   let list =driver.filter((driver) => driver.toLowerCase()==name2.toLowerCase())
+   return list ;
+
+}
+
+function fuzzyMatch (arr,str){
+ return arr.filter(
+    (po) =>  
+    po.toLowerCase().indexOf( str.toLowerCase())==0 )
+}
+function matchName(drivers,str){
+return drivers.filter((driver)=>
+driver.name==str
+)
+}
